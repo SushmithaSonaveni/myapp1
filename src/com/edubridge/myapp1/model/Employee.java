7eddf9eed09cc18142998a2cca7e08e9f086979c
SushmithaSonaveni <sushmithasonaveni@gmail.com>
2024-09-24 13:29:31 +0530

    Update Employee.java

--- src/com/edubridge/myapp1/model/Employee.java
+++ src/com/edubridge/myapp1/model/Employee.java
@@ -5,6 +5,8 @@ public class Employee {
 	private String employeeName;
 	private double employeeSalary;
 	private String employeeEmail;
+	private Long employeeMobile;
+	private String employeeGender;
 	
 	public int getEmployeeId() {
 		return employeeId;
